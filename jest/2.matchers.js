8038b40bfd25c20a7986e5d61e55003a5b768107
Prashant rawal <Prashantrawal619@gmail.com>
2019-09-18 17:13:03 +0530

    floating point test add

--- jest/2.matchers.js
+++ jest/2.matchers.js
@@ -66,3 +66,9 @@ test("two plus two", () => {
 });
 
 //For floating point equality, use toBeCloseTo instead of toEqual, because you don't want a test to depend on a tiny rounding error.
+
+test("adding floating point numbers", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
